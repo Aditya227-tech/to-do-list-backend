c65ab7e6c605a4f6990419ea96ef1b1760192b3c
Aditya227-tech <141914748+Aditya227-tech@users.noreply.github.com>
2024-12-15 15:45:18 +0000

    Updated

--- replit.nix
+++ replit.nix
@@ -1,5 +1,21 @@
 {pkgs}: {
   deps = [
-    pkgs.docker_26
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.python311Packages.virtualenv
+    pkgs.docker
+    pkgs.nodejs
+    pkgs.chromedriver
+    pkgs.google-chrome-beta
   ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.python311
+      pkgs.python311Packages.pip
+    ];
+    PYTHONBIN = "${pkgs.python311}/bin/python3.11";
+    LANG = "en_US.UTF-8";
+    STDERREDBIN = "${pkgs.stdenv.cc}/bin/stderr";
+    PROOT_CPUID = "genuine";
+  };
 }
